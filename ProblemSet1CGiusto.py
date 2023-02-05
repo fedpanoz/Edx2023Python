5ac162094c28f47d6631da34890c3161674f81aa
fedpanoz <federicopanozzo@gmail.com>
2023-02-05 23:25:01 +0100

    05.05

--- ProblemSet1CGiusto.py
+++ ProblemSet1CGiusto.py
@@ -4,17 +4,17 @@
 # In the case of ties, print the first substring. For example, if s = 'abcbcd', then your program should print
 # Longest substring in alphabetical order is: abc'''
 
-s = 'azcbobobegghakl'
 
-max_substring = ""
-current_substring = ""
-for i in range(len(s) - 1):
-    if s[i] <= s[i + 1]:
-        current_substring += s[i]
-    else:
-        current_substring += s[i]
-        if len(current_substring) > len(max_substring):
-            max_substring = current_substring
-        current_substring = ""
 
-print("Longest substring in alphabetical order is: " + max_substring)
+s = 'kyoxggekjkbhik'
+main_substring = ""
+for i in range(len(s)):
+    new_substring = s[i]
+    for j in range(i+1, len(s)):
+        if s[j] >= s[j-1]:
+            new_substring += s[j]
+        else:
+            break
+    if len(new_substring) > len(main_substring):
+        main_substring = new_substring
+print("Longest substring in alphabetical order is: " + main_substring)
