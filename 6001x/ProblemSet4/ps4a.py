5a23de02e3816fc25eaacc852d9a0318a5178a97
fedpanoz <federicopanozzo@gmail.com>
2023-03-09 20:37:30 +0100

    09.03

--- 6001x/ProblemSet4/ps4a.py
+++ 6001x/ProblemSet4/ps4a.py
@@ -280,8 +280,22 @@ def playGame(wordList):
  
     2) When done playing the hand, repeat from step 1    
     """
-    # TO DO ... <-- Remove this comment when you code this function
-    print("playGame not yet implemented.") # <-- Remove this line when you code the function
+    # TO DO... <-- Remove this comment when you code this function
+    hand = {}
+    while True:
+        userinput = input("Enter n to deal a new hand, r to replay the last hand, or e to end game: ")
+        if userinput == "n":
+            hand = dealHand(HAND_SIZE)
+            playHand(hand, wordList, HAND_SIZE)
+        elif userinput == "r":
+            if hand == {}:
+                print("You have not played a hand yet. Please play a new hand first!")
+            else:
+                playHand(hand, wordList, HAND_SIZE)
+        elif userinput == "e":
+            break
+        else:
+            print("Invalid command.")
    
 
 
