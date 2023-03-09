57096e258a1223740bec4466d4804ea6efaada7c
fedpanoz <federicopanozzo@gmail.com>
2023-03-09 21:13:23 +0100

    09.03

--- 6001x/ProblemSet4/ps4b.py
+++ 6001x/ProblemSet4/ps4b.py
@@ -124,12 +124,45 @@ def playGame(wordList):
 
     wordList: list (string)
     """
-    # TO DO... <-- Remove this comment when you code this function
-    print("playGame not yet implemented.") # <-- Remove this when you code this function
+    hand = {}
+    while True:
+        user_input = input("Enter n to deal a new hand, r to replay the last hand, or e to end game: ")
+        if user_input == 'e':
+            break
+        elif user_input == 'n':
+            hand = dealHand(HAND_SIZE)
+            while True:
+                user_input = input("Enter u to have yourself play, c to have the computer play: ")
+                if user_input == 'u':
+                    playHand(hand, wordList, HAND_SIZE)
+                    break
+                elif user_input == 'c':
+                    compPlayHand(hand, wordList, HAND_SIZE)
+                    break
+                else:
+                    print("Invalid command.")
+        elif user_input == 'r':
+            if hand == {}:
+                print("You have not played a hand yet. Please play a new hand first!")
+            else:
+                while True:
+                    user_input = input("Enter u to have yourself play, c to have the computer play: ")
+                    if user_input == 'u':
+                        playHand(hand, wordList, HAND_SIZE)
+                        break
+                    elif user_input == 'c':
+                        compPlayHand(hand, wordList, HAND_SIZE)
+                        break
+                    else:
+                        print("Invalid command.")
+        else:
+            print("Invalid command.")
+
 
         
 #
 # Build data structures used for entire session and play game
+
 #
 if __name__ == '__main__':
     wordList = loadWords()
