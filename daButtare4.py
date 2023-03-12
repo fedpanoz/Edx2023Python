27d4db054cbfcd8bd26a0a6ef346df794ca92841
fedpanoz <federicopanozzo@gmail.com>
2023-03-12 08:52:56 +0100

    09.03

--- daButtare4.py
+++ daButtare4.py
@@ -4,8 +4,22 @@ prova = [1, -2, 3.4, -45]
 #         L[x] = f(L[x])
 #     return L
 # print(applyToEach(prova, int))
-acca = map(abs, prova)
-print(acca)
-print(type(acca))
-for x in acca:
-    print(x)
+# acca = map(abs, prova)
+# print(acca)
+# print(type(acca))
+# for x in acca:
+#     print(x)
+def applyEachTo(L, x):
+    result = []
+    for i in range(len(L)):
+        result.append(L[i](x))
+    return result
+
+def square(a):
+    return a * a
+
+def halve(a):
+    return a / 2
+
+def inc(a):
+    return a + 1
