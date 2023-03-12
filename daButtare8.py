39c0e8a3da474bdeea633e2a2c71f117064df455
fedpanoz <federicopanozzo@gmail.com>
2023-03-12 17:10:00 +0100

    23.02

--- daButtare8.py
+++ daButtare8.py
@@ -1,10 +1,13 @@
-def applyToEach(L, f):
-    for i in range(len(L)):
-        L[i] = f(L[i])
-
-
-testList = [1, -4, 8, -9]
-
-applyToEach(testList, abs)
-print(testList)
-
+# def applyToEach(L, f):
+#     for i in range(len(L)):
+#         L[i] = f(L[i])
+#
+#
+# testList = [1, -4, 8, -9]
+#
+# applyToEach(testList, abs)
+# print(testList)
+#
+animals = {'a': 'aardvark', 'b': 'baboon', 'c': 'coati'}
+animals['d'] = 'donkey'
+print(len(animals['a']))
