4107431029b8a6d69f8dbf7451de17245a5c9f87
fedpanoz <federicopanozzo@gmail.com>
2023-03-15 06:43:51 +0100

    09.03

---
+++ daButtare9.py
@@ -0,0 +1,6 @@
+pippo = {'Andy': 9, 'Fedez': 10, 'Zucco': 78, 'Prino': 3}
+valori = pippo.values()
+print(type(valori),  valori)
+chiavi = pippo.keys()
+print(type(chiavi), chiavi)
+
---
+++ songs.py
@@ -0,0 +1,40 @@
+def lyrics_to_frequencies(lyrics):
+    myDict = {}
+    for word in lyrics:
+        if word in myDict:
+            myDict[word] += 1
+        else:
+            myDict[word] = 1
+    return myDict
+
+she_loves_you = ["she", "loves", "you", "yeah", "yeah", "yeah",
+                    "she", "loves", "you", "yeah", "yeah", "yeah",
+                    "she", "loves", "you", "yeah", "yeah", "yeah",
+                    "she", "loves", "you", "but", "who", "do", "you",
+                    "love", "yeah", "yeah", "yeah"]
+
+beatles = lyrics_to_frequencies(she_loves_you)
+def most_common_words(freqs):
+    values = freqs.values()
+    best = max(values)
+    words = []
+    for k in freqs:
+        if freqs[k] == best:
+            words.append(k)
+    return (words, best)
+
+def words_often(freqs, minTimes):
+    result = []
+    done = False
+    while not done:
+        temp = most_common_words(freqs)
+        if temp[1] >= minTimes:
+            result.append(temp)
+            for w in temp[0]:
+                del(freqs[w])
+        else:
+            done = True
+    return result
+
+#print(words_often(beatles, 5))
+print(most_common_words(beatles))
